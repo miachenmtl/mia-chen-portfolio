a4948145bfceccd2edc855b8c554a6c262cdf7b3
Mia Chen <mia.chen.mtl@gmail.com>
2021-08-23 13:39:02 -0400

    create empty components and pages, install typescript

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from "react";
+
+export default function Hero(props): ReactElement {
+  return (
+    <div>
+      <h1>Mia Chen</h1>
+      <h2>Front-End Web Dev</h2>
+      <h3>React, JavaScript, HTML5, CSS3, JAMstack</h3>
+      {/* one sentence */}
+    </div>
+  );
+}
